dbeb281567b7e006ca234880d96375897ca52dd4
Kimberly Salas <salask0104@gmail.com>
2021-06-24 16:33:48 -0700

    added .env and to the index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes')
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
 
 router.use('/user', userRoutes);
-router.use('/post', postRoutes)
+router.use('/post', postRoutes);
+router.use('/comments', commentRoutes);
+
 
 module.exports = router;
 
